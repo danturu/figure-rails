31283e9f6f7da035e7264832f05ad5678951db5d
Dimitri Rosenberg <di@radiant.fm>
2015-09-20 18:26:38 +0300

    create rails app with mongo and es6

---
+++ Gemfile
@@ -0,0 +1,27 @@
+ruby '2.2.2'
+
+source 'https://rubygems.org'
+
+gem 'active_model_serializers', '0.9.3'
+gem 'bourbon',                  '4.2.2'
+gem 'bower-rails',              '0.9.2'
+gem 'coffee-rails',             '4.1.0'
+gem 'devise',                   '3.4.1'
+gem 'mongoid',                  '4.0.2'
+gem 'puma',                     '2.11.2'
+gem 'rack-cors',                '0.3.1', require: 'rack/cors'
+gem 'rails',                    '4.2.1'
+gem 'redis',                    '3.2.1'
+gem 'sass-rails',               '5.0.3'
+gem 'sprockets-es6',            '0.8.0'
+gem 'uglifier',                 '2.7.1'
+
+group :production do
+  gem 'rails_12factor', '0.0.3'
+end
+
+group :development, :test do
+  gem 'dotenv-rails', '2.0.1'
+  gem 'rspec-rails',  '3.2.1'
+end
+
---
+++ config/application.rb
@@ -0,0 +1,70 @@
+require File.expand_path("../boot", __FILE__)
+
+require "active_model/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module FigureServer
+  class Application < Rails::Application
+    VERSION = "0.1.0"
+
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += [config.root.join("lib"), config.root.join("app/models/users")]
+
+    # Configure default locale.
+    config.i18n.default_locale = :en
+
+    # Enable locale fallbacks for I18n.
+    config.i18n.fallbacks = true
+
+    # Skip validation of locale.
+    config.i18n.enforce_available_locales = false
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Precompile fonts.
+    config.assets.precompile += %w(*.woff2 *.eot *.ttf *.woff *.svg)
+
+    # This is how we send emails.
+
+    config.action_mailer.default_options = {
+      from: "Figure <no-reply@figure-app.com>"
+    }
+
+    config.action_mailer.default_url_options = {
+      host: ENV["ACTIONMAILER_HOST"]
+    }
+
+    config.action_mailer.smtp_settings = {
+      address:              "smtp.mandrillapp.com",
+      authentication:       "login",
+      domain:               "figure-app.com",
+      port:                 "587",
+      enable_starttls_auto: true,
+      password:             ENV["MANDRILL_PASSWORD"],
+      user_name:            ENV["MANDRILL_USERNAME"],
+    }
+
+    # Browsers only render web fonts served with the appropriate “Access-Control-Allow-Origin” header.
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins  "*"
+        resource "/assets/*", headers: :any, methods: :get
+      end
+    end
+  end
+end
+
+module Sprockets::Rails::Helper
+  require Rails.root.join("app/helpers/application_helper.rb")
+  include ApplicationHelper
+end
