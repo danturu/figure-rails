f84a22ddb18f7d510599f7c80d8be044b66134bb
Dimitri Rosenberg <di@radiant.fm>
2015-09-30 17:33:06 +0300

    add inline svg helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def embedded_svg(filename, options={})
+    doc = Nokogiri::HTML::DocumentFragment.parse(Rails.application.assets.find_asset(filename).to_s.force_encoding("UTF-8"))
+    svg = doc.at_css "svg"
+
+    if options[:class].present?
+      svg["class"] = options[:class]
+    end
+
+    raw doc
+  end
 end
+
