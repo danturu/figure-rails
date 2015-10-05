632ac972bb050007da399c4e2c837550f50b4b6d
Dimitri Rosenberg <di@radiant.fm>
2015-10-05 22:51:56 +0300

    remove sprockets

--- Gemfile
+++ Gemfile
@@ -4,15 +4,12 @@ source 'https://rubygems.org'
 
 gem 'active_model_serializers', '>= 0.10.0.rc'
 gem 'bourbon',                  '4.2.5'
-gem 'browserify-rails', github: 'rosendi/browserify-rails'
 gem 'devise',                   '3.5.2'
 gem 'mongoid',                  '5.0.0'
 gem 'puma',                     '2.14.0'
 gem 'rack-cors',                '0.4.0', require: 'rack/cors'
 gem 'rails',                    '4.2.4'
 gem 'redis',                    '3.2.1'
-gem 'sass-rails',               '5.0.4'
-gem 'uglifier',                 '2.7.2'
 
 group :production do
   gem 'rails_12factor', '0.0.3'
--- config/application.rb
+++ config/application.rb
@@ -4,30 +4,15 @@ require "active_model/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 
 module FigureServer
-  class TypeScriptTemplate < Tilt::Template
-    self.default_mime_type = "application/javascript"
-
-    def prepare
-    end
-
-    def evaluate(scope, locals, &block)
-      @output ||= data
-    end
-  end
-
   class Application < Rails::Application
     VERSION = "0.1.0"
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += [config.root.join("lib")]
-
-    # Custom directories with assets.
-    config.assets.paths += [config.root.join("node_modules"), config.root.join("vendor/assets/javascripts/typings")]
 
     # Configure default locale.
     config.i18n.default_locale = :en
@@ -44,23 +29,7 @@ module FigureServer
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    # Precompile fonts.
-    config.assets.precompile += %w(*.woff2 *.eot *.ttf *.woff *.svg)
-
-    # TypeScript compiling.
-
-    assets.register_engine '.ts', TypeScriptTemplate
-
-    config.annotations.register_extensions("ts") do |annotation|
-      /#\s*(#{annotation}):?\s*(.*)$/
-    end
-
-    config.browserify_rails.force = ->(file) { File.extname(file) == ".ts" }
-
-    config.browserify_rails.commandline_options = "-p [tsify --module commonjs --target es5 --noImplicitAny --experimentalDecorators --emitDecoratorMetadata] --extension=.ts"
-
     # Email sending.
-
     config.action_mailer.default_options = {
       from: "Figure <no-reply@figure-app.com>"
     }
@@ -80,7 +49,6 @@ module FigureServer
     }
 
     # Browsers only render web fonts served with the appropriate “Access-Control-Allow-Origin” header.
-
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins  "*"
@@ -88,9 +56,4 @@ module FigureServer
       end
     end
   end
-end
-
-module Sprockets::Rails::Helper
-  require Rails.root.join("app/helpers/application_helper.rb")
-  include ApplicationHelper
 end
